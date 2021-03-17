47596d8c4779d64a25eb7d57f61b8ee84d5f9cb6
williamevann <c14180222@john.petra.ac.id>
2021-03-17 09:27:32 +0700

    commitAkhir

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FotoService } from '../foto.service';
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +8,13 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  constructor(public fotoservice: FotoService) {}
 
+  tambahfoto(){
+    this.fotoservice.tambahfoto();
+  }
+
+  async ngOnInit(){
+    await this.fotoservice.loadFoto();
+  }
 }
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -9,4 +9,23 @@ export class Tab2Page {
 
   constructor() {}
 
+  gameWin : boolean;
+  randomNumber : Number;
+  angkaInput : Number;
+
+  
+  randomCheck()
+  {
+    this.randomNumber = Math.floor(Math.random() * (5 - 1) + 1);
+
+    if(this.angkaInput == this.randomNumber)
+    {
+      this.gameWin = true;
+    }
+    else{
+      this.gameWin = false;
+    }
+  }
+
+  
 }
